9367336805cb9cbe0b6dff9474897fb8859e5b92
Nguyen Tien Thuan <thuannguyen20041028@gmail.com>
2025-07-03 16:02:48 +0700

    update code

--- config/index.ts
+++ config/index.ts
@@ -35,7 +35,7 @@ export const siteConfig: Metadata = {
     "css",
   ] as Array<string>,
   authors: {
-    name: "Sanidhya Kumar Verma",
-    url: "https://github.com/sanidhyy",
+    name: "Thunggg",
+    url: "https://github.com/Thunggg",
   },
 } as const;
